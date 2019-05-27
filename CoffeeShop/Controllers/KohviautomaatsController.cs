32159e1cc25615d28a80df860fe575247fcc0993
mihkelaa <mihkelader01@gmail.com>
2019-05-27 14:24:44 +0300

    Add project files.

---
+++ CoffeeShop/Controllers/KohviautomaatsController.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using CoffeeShop.Models;
+
+namespace CoffeeShop.Controllers
+{
+    public class KohviautomaatsController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        //GET: Kohviautomaats
+        public ActionResult Index()
+        {
+            return View(db.Kohviautomaats.ToList());
+        }
+
+        // GET: Kohviautomaats/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Kohviautomaat kohviautomaat = db.Kohviautomaats.Find(id);
+            if (kohviautomaat == null)
+            {
+                return HttpNotFound();
+            }
+            return View(kohviautomaat);
+        }
+
+        // GET: Kohviautomaats/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Kohviautomaats/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,JoogiNimi,TopsePakis,TopseJuua")] Kohviautomaat kohviautomaat)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Kohviautomaats.Add(kohviautomaat);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(kohviautomaat);
+        }
+
+        // GET: Kohviautomaats/Edit/5
+        
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Kohviautomaat kohviautomaat = db.Kohviautomaats.Find(id);
+            if (kohviautomaat == null)
+            {
+                return HttpNotFound();
+            }
+            return View(kohviautomaat);
+        }
+
+        // POST: Kohviautomaats/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,JoogiNimi,TopsePakis,TopseJuua")] Kohviautomaat kohviautomaat)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(kohviautomaat).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(kohviautomaat);
+        }
+
+        // GET: Kohviautomaats/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Kohviautomaat kohviautomaat = db.Kohviautomaats.Find(id);
+            if (kohviautomaat == null)
+            {
+                return HttpNotFound();
+            }
+            return View(kohviautomaat);
+        }
+
+        // POST: Kohviautomaats/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Kohviautomaat kohviautomaat = db.Kohviautomaats.Find(id);
+            db.Kohviautomaats.Remove(kohviautomaat);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+        [Authorize]
+        public ActionResult Haldus([Bind(Include = "Id,JoogiNimi,TopsePakis,TopseJuua")] Kohviautomaat kohviautomaat)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(kohviautomaat).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(kohviautomaat);
+        }
+        public ActionResult Telli()
+        {
+            return View(db.Kohviautomaats.ToList());
+        }
+       
+
+    }
+}
---
+++ CoffeeShop/Models/Kohviautomaat.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace CoffeeShop.Models
+{
+    public class Kohviautomaat
+    {
+
+        public int Id { get; set; }
+        public string JoogiNimi { get; set; }
+        public int TopsePakis { get; set; } = 50;
+
+        [Range(0, 100)]
+        public int TopseJuua { get; set; } = 0;
+
+        [Range(1,2)]
+        public int PakkeLisatud { get; set; }
+
+    }
+}
